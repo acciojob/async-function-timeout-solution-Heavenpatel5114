d986e1f19ba605b89b554ccffd4413c43f12c2c8
Heavenpatel5114 <113304844+Heavenpatel5114@users.noreply.github.com>
2023-04-28 11:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ const delay = document.getElementById("delay");
 const btn = document.getElementById("btn");
 const output = document.getElementById("output");
 
-//your code here
+// write your code here
 async function showMessage() {
   const message = text.value;
   const delayVal = delay.value;
   await new Promise((resolve) => setTimeout(resolve, delayVal));
   output.innerText = message;
 }
-showMessage().then(()=>{
-	output.innerHTML = text.value;
-});
+
 btn.addEventListener("click", showMessage);
